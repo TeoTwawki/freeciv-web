cf90a4f29f3fac856dc1f0e0cfd69839e75c7994
Marko Lindqvist <cazfi74@gmail.com>
2024-05-03 23:58:40 +0300

    Server: Backport 0061-savegame-Correct-loading-last-turn-change-time.patch
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- freeciv/apply_patches.sh
+++ freeciv/apply_patches.sh
@@ -33,6 +33,9 @@
 # 0048-Handle-CoastStrict-units-correctly-on-city-removal.patch
 #   Fix to unit placement after city destruction
 #   RM #525
+# 0061-savegame-Correct-loading-last-turn-change-time.patch
+#   Savegame loading fix
+#   RM #545
 
 # Not in the upstream Freeciv server
 # ----------------------------------
@@ -65,6 +68,7 @@ declare -a PATCHLIST=(
   "backports/0049-Trigger-action-system-when-client-requests-activity-"
   "backports/0060-Check-C23-nullptr-usability-as-a-sentinel"
   "backports/0048-Handle-CoastStrict-units-correctly-on-city-removal"
+  "backports/0061-savegame-Correct-loading-last-turn-change-time"
   "RevertAmplio2ExtraUnits"
   "meson_webperimental"
   "metachange"
